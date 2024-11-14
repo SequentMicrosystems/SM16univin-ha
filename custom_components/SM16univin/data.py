cbe379a7ca76e9e59185057dfcd390598fe6aa24
3lv <cristian.vlad.vladutu@gmail.com>
2024-11-14 21:25:51 +0200

    Updated from template (using script)

--- custom_components/SM16univin/data.py
+++ custom_components/SM16univin/data.py
@@ -22,15 +22,6 @@ SM_MAP = {
                 },
         }
     },
-    "datetime": {
-        "rtc": {
-                "chan_no": 1,
-                "com": {
-                    "get": "get_rtc",
-                    "set": "set_rtc",
-                },
-        },
-    },
     "switch": {
         "dig_cnt_en": {
                 "chan_no": 16,
@@ -67,6 +58,15 @@ SM_MAP = {
                 "uom": "Ohm",
                 "com": {
                     "get": "get_r10k_in",
+                },
+        },
+    },
+    "datetime": {
+        "rtc": {
+                "chan_no": 1,
+                "com": {
+                    "get": "get_rtc",
+                    "set": "set_rtc",
                 },
         },
     },
